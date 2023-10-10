7ac90d0eb9f9728b6005fe2c61078ddedd568bc4
Daniel Boubeta Portela <dani-bp@hotmail.es>
2023-10-10 14:51:49 +0200

    License info added on each file

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,29 @@
+#
+#  components/__init__.py - FreeDSM support for Raspberry PI
+#
+#
+#  FreeDSM:
+#  Copyrigth (C) 2022  Daniel Boubeta Portela
+#                      José Carlos Dafonte Vázquez
+#                      
+#  University of A Coruña (UDC), Galicia, Spain
+#  Developed as a part of the Gaia4Sustainability (G4S) project
+#  <http://gaia4sustainability.eu/>.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from .sensors.sqm import SQM
 from .sensors.tsl2591 import TSL2591
 from .sensors.aht10 import AHT10
